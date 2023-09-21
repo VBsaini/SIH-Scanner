de6ce218b5ffc52ed62f4f2e534236f076a61d6d
Naman Arora <namanarora1022@gmail.com>
2023-09-21 21:04:40 +0530

    fix: button padding removed useless comments

--- components/home/Home.js
+++ components/home/Home.js
@@ -11,30 +11,21 @@ export default function Home() {
       <View style={styles.overlay} />
       <Image
         style={styles.logo}
-        // source={{
-        //   uri: "https://images.unsplash.com/photo-1518531933037-91b2f5f229cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1854&q=80",
-        // }}
         src="https://images.unsplash.com/photo-1518531933037-91b2f5f229cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1854&q=80"
       />
       <View style={styles.innerContainer}>
         <Text style={{ color: "#fff" }}>
           Open up App.js yello asdto start working on your app!
         </Text>
-        <View style={{ marginHorizontal: 40, marginTop: 20, height: 50 }}>
+        <View style={{ marginHorizontal: 40, marginTop: 20, height: 36 }}>
           <Button
             style={{
               backgroundColor: "#327a14",
-              // paddingVertical: 10,
               flex: 1,
-              // alignSelf: "center",
-              // minHeight: 50,
             }}
-            //   titleStyle={{ fontSize: 20 }}
             titleStyle={{
               fontSize: 20,
-              // height: 50,
               alignSelf: "center",
-              //   marginVertical: 10,
             }}
             size="Large"
             title="Scan"
@@ -43,17 +34,9 @@ export default function Home() {
         <View style={{ marginHorizontal: 40, marginTop: 20 }}>
           <Button
             color="#327a14"
-            style={
-              {
-                // paddingVertical: 10,
-                // alignSelf: "center",
-              }
-            }
-            //   titleStyle={{ fontSize: 20 }}
             titleStyle={{
               fontSize: 20,
               alignSelf: "center",
-              //   marginVertical: 10,
             }}
             size="large"
             title="How to Scan"
@@ -74,7 +57,6 @@ const styles = StyleSheet.create({
     position: "relative",
   },
   innerContainer: {
-    // marginHorizontal: 50,
     zIndex: 10,
   },
   logo: {
