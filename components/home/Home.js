df5ff096936460170df0fe6f13d6bdbc7f132ada
Naman Arora <namanarora1022@gmail.com>
2023-09-21 21:28:30 +0530

    feat: app name with custom font

--- components/home/Home.js
+++ components/home/Home.js
@@ -3,11 +3,27 @@ import { StyleSheet, Text, View, Image } from "react-native";
 import { Button } from "@react-native-material/core";
 import { Dimensions } from "react-native";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { loadAsync } from "expo-font";
+import { useEffect, useState } from "react";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
+const customFonts = {
+  DancingScript: require("../../assets/fonts/DancingScript-Bold.ttf"),
+};
 
 export default function Home() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await loadAsync(customFonts);
+      setFontsLoaded(true);
+    })();
+  }, []);
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
       <View style={styles.overlay} />
@@ -16,7 +32,8 @@ export default function Home() {
         src="https://images.unsplash.com/photo-1518531933037-91b2f5f229cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1854&q=80"
       />
       <View style={styles.innerContainer}>
-        <Text style={{ color: "#fff" }}>
+        <Text style={styles.heading}>Zephire</Text>
+        <Text style={styles.text}>
           Click the scan button below to scan your image
         </Text>
         <View style={{ marginHorizontal: 40, marginTop: 20, height: 36 }}>
@@ -81,5 +98,18 @@ const styles = StyleSheet.create({
     top: -10,
     zIndex: 5,
     backgroundColor: "rgba(0, 0, 0, 0.4)",
+  },
+  heading: {
+    color: "gold",
+    fontSize: 80,
+    textAlign: "center",
+    marginBottom: 20,
+    fontFamily: "DancingScript",
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "500",
   },
 });
