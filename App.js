270ba554337c0bd3548684a1c37706ca0565b86d
Naman Arora <namanarora1022@gmail.com>
2023-09-22 18:03:27 +0530

    refactor: App.js clean up remove Details Screen commented out view unsed styles

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StyleSheet, View } from "react-native";
-import { Text, Image } from "react-native";
-import Home from "./components/home/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+// Screens
+import Home from "./components/home/Home";
+
 const Stack = createNativeStackNavigator();
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+
 function App() {
   return (
-    // <View style={styles.container}>
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
@@ -25,16 +18,7 @@ function App() {
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
-    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 export default App;
