273b3be4303df747a9f56d67fcd6d162b1a4d9e6
Naman Arora <namanarora1022@gmail.com>
2023-09-22 22:23:02 +0530

    refactor: screens folder

--- App.jsx
+++ App.jsx
@@ -2,8 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Screens
-import Home from "./components/home/Home";
-import Plant from "./components/plant/Plant";
+import { Home, Plant } from "./screens";
 
 const Stack = createNativeStackNavigator();
 
